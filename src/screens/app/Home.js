885c9050dcb833f851bad64d3c8ee5b1acdc1a71
PTTZSURV <patotanzania@gmail.com>
2025-09-18 13:18:43 +0300

    Update Home.js

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
 
   // Fetch surveys data
   useEffect(() => {
-    fetch('https://victonictechnologies.github.io/SurveysDetails/surveys.json')
+    fetch('https://pttzsurv.github.io/eranke/surveys.json')
       .then((response) => response.json())
       .then((data) => {
         setSurveysData(data.surveys);
